a882063cab31058cdc5ead54742444b8f2e5d3d0
xhui <xhui.xiao@gmail.com>
2015-12-12 21:39:52 +0800

    Add stock.h.

---
+++ include/stock.h
@@ -0,0 +1,16 @@
+
+/*
+ * Copyright(c) 2015 Xiao Hui <xhui.xiao@gmail.com>. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ */
+
+ struct OHLC {
+    double open;
+    double close;
+    double high;
+    double close;
+ }
